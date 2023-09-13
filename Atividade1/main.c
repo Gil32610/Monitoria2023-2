eefed8928db715474349e9565f0bffcd368248ac
GIL32610 <gil.00000032610@unicap.br>
2023-09-13 18:33:34 -0300

    Stack local variable needs fix

--- Atividade1/main.c
+++ Atividade1/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-
+void repeatedPositions(int *p, int size);
+int *findPositions(int values[], int size, int key);
 int main()
 {
     int repetidos[] = {10, 20, 40, 10, 10, 10, 50};
@@ -12,25 +13,36 @@ int main()
 
 void repeatedPositions(int *p, int size)
 {
+    printf("Tamanho do vetor: %d\n", size);
     printf("Valor encontrado na posicao: ");
     for (int i = 0; i < size; i++)
     {
+        printf("\nIteracao: %d\n", i);
+        printf("valor no endereço apontado por ponteiro: %d\n", (p[i]));
         if (*(p + i) == 1)
-            printf("%d%s", i != size - 1 ? *(p + i) + ", " : ".\n");
+        {
+            printf("%d%s", *(p + i), i != size - 1 ? ", " : ".\n");
+        }
     }
 }
 
 int *findPositions(int values[], int size, int key)
 {
     int sequence[size];
-    int index;
     for (int i = 0; i < size; i++)
     {
+
         if (values[i] == key)
         {
-            sequence[index++] = 1;
+            printf("valor encontrado em %d é %d \n", i, values[i]);
+            sequence[i] = 1;
+            continue;
         }
-
-        return sequence;
+        sequence[i] = 0;
     }
+    for (int i = 0; i < size; i++)
+    {
+        printf("valor no vetor repetido %d\n", sequence[i]);
+    }
+    return sequence;
 }
