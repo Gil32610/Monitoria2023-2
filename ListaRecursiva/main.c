3054e98e3b6bb8746f16da6639d4cf1226f4da2d
GIL32610 <gil.00000032610@unicap.br>
2023-09-13 17:36:10 -0300

    Reverse de String

--- ListaRecursiva/main.c
+++ ListaRecursiva/main.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
 
-
-void reverseString(char word [], int size);
-void revertingString(char sequence [],int beginning, int last);
+void reverseString(char word[], int size);
+void revertingString(char sequence[], int beginning, int last);
 int main()
 {
-char sequence [] = "abcd";
-printf("%s\n", sequence);  
-int size = sizeof(sequence)/1;
-printf("%d",size);
-reverseString(sequence,size);
-printf("%s\n", sequence);
+    char sequence[] = "survived";
+    printf("%s\n", sequence);
+    int size = sizeof(sequence) - 1;
+    reverseString(sequence, size);
+    printf("%s\n", sequence);
     return 0;
 }
 
- void reverseString(char word[], int size){
-printf("%d", size);
-    revertingString( word,0,size-1);
+void reverseString(char word[], int size)
+{
+    printf("Tamanho da sequencia: %d\n", size);
+    revertingString(word, 0, size - 1);
 }
 
-
-void revertingString(char sequence[],int beginning, int last){
-if(beginning >= last){
-    return;
-}
-char aux = sequence[beginning];
-sequence[beginning] = sequence[last];
-sequence[last] = aux;
-revertingString(sequence,beginning+1, last-1);
+void revertingString(char sequence[], int beginning, int last)
+{
+    if (beginning >= last)
+    {
+        return;
+    }
+    char aux = sequence[beginning];
+    sequence[beginning] = sequence[last];
+    sequence[last] = aux;
+    revertingString(sequence, beginning + 1, last - 1);
 }
