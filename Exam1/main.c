e95de9e1b4d34cf3c5dbfe17f892ae9daa3370fe
GIL32610 <gil.00000032610@unicap.br>
2023-09-19 09:18:01 -0300

    exam 1 recursion and rbs

---
+++ Exam1/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdbool.h>
+ int steps = 0;
+int main(){
+
+    return 0;
+}
+
+
+bool primeNumber(int n, int i){
+if(i== n || n<=2)
+return true;
+if(n%i== 0){
+    return false;
+}
+return primeNumber(n,i+1);
+}
+
+int binarySearchWithSteps(int low,int mid,int high, int key, int*array){
+    mid = (high+low)/2;
+
+    if(low>=high)
+        return -1;
+    
+    if(getItem(array[mid])==key)
+        return mid;
+    
+
+    if(getItem(array[mid])>key)
+        return binarySearchWithSteps(mid+1, mid, high, key, array);
+    
+    if(getItem(array[mid])>key)
+        return binarySearchWithSteps(low, mid, mid-1, key, array);
+    
+
+}
+
+
+int getItem(int*position){
+    steps++;
+    return *position;
+}
+
+
+
+
+
