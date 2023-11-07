7335dacef7557a91aae23eefd4e67457d980a233
GIL32610 <gil.00000032610@unicap.br>
2023-11-07 17:37:50 -0300

    function to get characters in matring

--- Matring/main.c
+++ Matring/main.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+int currentPosition = 0;
 int getFirst(int *matringMatrix, int columns);
 int getLast(int *matringMatrix, int columns);
-
+int* characters(int* matringMatrix, int columns);
+void setChar (int* word, int value);
 int main()
 {
     int row = 4;
@@ -10,14 +13,10 @@ int main()
     int matring[4][5] = {{4, 1, 8, 0, 5},
                          {9, 9, 9, 3, 4},
                          {3, 9, 1, 2, 7},
-                         {
-                             2,
-                             3,
-                             6,
-                             5,
-                             9,
-                         }};
+                         {2, 3, 6, 5, 9,}};
     int first = getFirst((int *)matring, 5);
+    int last = getLast((int*)matring,5);
+    int* word = characters((int*)matring, 5);
     printf("%d", first);
     return 0;
 }
@@ -50,4 +49,28 @@ int getLast(int *matringMatrix, int columns)
         }
     }
     return last;
+}
+int* characters(int* matringMatrix,int columns){
+    int* word = (int*) malloc((sizeof(int))*3);
+    int decimal =1000;
+    int mi =0;
+    for(int i =1; i< 4;i++){
+        for(int j = 0; j< columns-1;j++){
+          printf("Cell value is: %d\n",*((matringMatrix + (j * 5)) + i));
+          mi += *((matringMatrix + (j * 5)) + i) * decimal;
+            decimal/=10;
+           if(decimal<=0) break;  
+        }
+        printf("Mi value is: %d\n",mi);
+        setChar(word,mi);
+        decimal =1000;
+      mi =0;
+    }
+    currentPosition = 0;
+    return word;
+
+}
+
+void setChar (int* word, int value){
+    word[currentPosition++] = value;
 }
