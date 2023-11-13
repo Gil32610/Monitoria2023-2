f60f0ab8f72026d14c033521f84d3e52caa7c6a1
GIL32610 <gil.00000032610@unicap.br>
2023-11-13 15:55:53 -0300

    square root using binary search

---
+++ BinarySearch/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+//This program will take as input a double value then output its square root
+
+//Algorithm used: Binary Search
+double calculateSquareRoot(double number);
+
+double binarySearch(double low, double number, double high);
+
+int main(){
+double number;
+printf("Insert a number: \n");
+scanf("%lf", &number);
+
+double squareRoot = calculateSquareRoot(number);
+printf("Square root of %lf is: %lf",number,squareRoot);
+
+
+}
+
+double calculateSquareRoot(double number){
+
+
+return binarySearch(1,number,number);
+
+}
+
+double binarySearch(double low,double number,double high){
+    double mid = high+low/2;
+
+    double square = (mid*mid);
+
+    if(fabs(square) == number){
+
+        printf("the squere root of %lf is: %lf",number,mid);
+        return mid;
+    }
+    else if(fabs(square) > number){
+        return binarySearch(low, number, mid);
+    }
+    else if(fabs(square) < number){
+        return binarySearch(mid, number, high);
+    }
+    
+
+}
+
+
+
+
+
+
+
+
